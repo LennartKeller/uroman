35eb5c9d0ef1e10556e8082537667fb02baf9b41
Lennart Keller <lennartkeller@gmail.com>
2024-07-30 11:37:39 +0200

    add test for chinese characters

---
+++ test_chinese.py
@@ -0,0 +1,11 @@
+import uroman as ur
+
+uroman = ur.Uroman()
+
+
+def test_bad_string():
+    bad_string = "前三个月的focus的经营率的百分之多少或者是生命周期的focus经营率的百分之多少前者点位高一些或者点位低一些然后我们内部核算差不多这个点位应该是能擦过我们手臂的倍量就ok了这样所以我们我觉得没问题我觉得就这么写就是nre跟认证的费用是要在项目启动开始之前我要收到对吧然后关于倍量的成本的问题我们需要再拿到"
+    romanized_bad_string = uroman.romanize_string(bad_string)
+    if romanized_bad_string:
+        return True
+    return False
